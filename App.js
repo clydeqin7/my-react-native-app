4225fb3ebe835bdc391c53956fdf38cde04e4b20
Aqin7 <clydeqin7@foxmail.com>
2018-11-14 18:03:06 +0800

    tab引入ICON测试

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { createSwitchNavigator, createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 import SignInScreen from './src/screen/SignIn'
 import HomeScreen from './src/screen/Home'
@@ -5,11 +6,19 @@ import SettingsScreen from './src/screen/Settings'
 import DetailsScreen from './src/screen/Details'
 import AuthLoadingScreen from './src/screen/AuthLoading'
 import OtherScreen from './src/screen/OtherScreen'
+import Icon from 'react-native-vector-icons/AntDesign';
 
-const HomeStack = createStackNavigator({
-    Home: HomeScreen,
-    Details: DetailsScreen,
-}, { headerMode: 'none', });
+const HomeStack = createStackNavigator(
+    {
+        Home: HomeScreen,
+        Details: DetailsScreen,
+    },
+    {
+        headerMode: 'none',
+        // navigationOptions: ({ navigation }) => ({
+        //     title: '主页',
+        // }),
+    });
 
 const SettingsStack = createStackNavigator({
     Settings: SettingsScreen,
@@ -21,7 +30,38 @@ const OtherStack = createStackNavigator({
     Details: DetailsScreen,
 }, { headerMode: 'none', });
 
-const AppStack = createBottomTabNavigator({ Home: HomeStack, Settings: SettingsStack, Other: OtherStack });
+const AppStack = createBottomTabNavigator(
+    {
+        Home: {
+            screen: HomeStack,
+        },
+        '消息': SettingsStack,
+        '我的': OtherStack
+    },
+    {
+        // navigationOptions: ({ navigation }) => ({
+        //     tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        //         const { routeName } = navigation.state;
+        //         let iconName;
+        //         if (routeName === 'Home') {
+        //             iconName = 'home';
+        //         } else if (routeName === '消息') {
+        //             iconName = 'message';
+        //         } else if (routeName === '我的') {
+        //             iconName = 'setting'
+        //         }
+        //
+        //         // You can return any component that you like here! We usually use an
+        //         // icon component from react-native-vector-icons
+        //         return <Icon name={iconName} size={horizontal ? 20 : 25} color={tintColor} />;
+        //     },
+        // }),
+        tabBarOptions: {
+            activeTintColor: '#3b5998',
+            inactiveTintColor: '#cccccc'
+        },
+    }
+);
 const AuthStack = createStackNavigator({ SignIn: SignInScreen }, { headerMode: 'none', });
 
 export default createSwitchNavigator(
--- src/screen/Home.js
+++ src/screen/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {Text, View} from "react-native";
+import Icon from 'react-native-vector-icons/AntDesign';
 
 export default class HomeScreen extends React.Component {
     render() {
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                 <Text>Home!</Text>
+                <Icon name="home" size={30} color="#900" />
             </View>
         );
     }
